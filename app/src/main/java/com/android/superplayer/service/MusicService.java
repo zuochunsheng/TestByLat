86b745b34c3106a613a3c5306a498587e8788d69
zuochunsheng <1059737741@qq.com>
2018-09-05 16:55:01 +0800

    自动播放下一曲

--- app/src/main/java/com/android/superplayer/service/MusicService.java
+++ app/src/main/java/com/android/superplayer/service/MusicService.java
@@ -24,28 +24,44 @@ public class MusicService extends Service {
     private MediaPlayer player = new MediaPlayer();
     private MusicResult music;
 
-    private int state = 0x11 ; // 0x11 : 为第一次播放歌曲  ；  0x12 : 暂停 ； 0x13  继续播放
+    private int state = 0x11; // 0x11 : 为第一次播放歌曲  ；  0x12 : 暂停 ； 0x13  继续播放
+
+    private int curposition;
+    private int duration;
+
     @Override
     public void onCreate() {// 进行初始化操作
         MyBroadcastReceiver receiver = new MyBroadcastReceiver();
         IntentFilter intentFilter = new IntentFilter(flag_service);
-        registerReceiver(receiver,intentFilter);
+        registerReceiver(receiver, intentFilter);
 
 
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Intent intent = new Intent("com.android.superplayer.Activity");
+                intent.putExtra("over", true);
+
+                sendBroadcast(intent);
+                curposition = 0 ;
+                duration = 0;
+
+            }
+        });
         super.onCreate();
 
 
     }
 
-    public class MyBroadcastReceiver extends BroadcastReceiver{
+    public class MyBroadcastReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             int newmusic = intent.getIntExtra("newmusic", -1);// 1 新音乐
 
 
-            if(newmusic != -1 ){
+            if (newmusic != -1) {
                 music = (MusicResult) intent.getSerializableExtra("music");
-                if(music != null){
+                if (music != null) {
                     playMusic(music);
                     state = 0x12;
                     // 将当前状态发送给 activity
@@ -55,9 +71,9 @@ public class MusicService extends Service {
             }
 
             int isPlay = intent.getIntExtra("isPlay", -1);//  三种状态
-            if (isPlay != -1){
+            if (isPlay != -1) {
                 //0x11 : 为第一次播放歌曲  ；  0x12 : 暂停 ； 0x13  继续播放
-                switch (state){
+                switch (state) {
                     case 0x11:
                         music = (MusicResult) intent.getSerializableExtra("music");
                         playMusic(music);
@@ -84,24 +100,34 @@ public class MusicService extends Service {
             }
 
 
+            int progress = intent.getIntExtra("progress", -1);
+            if (progress != -1) {
+
+                curposition = (int) (progress * 1.0 / 100 * duration);// 将当前位置转换成毫秒
+
+                player.seekTo(curposition);
+
+            }
+
 
         }
     }
-    private void sendBroadToActivity(){
+
+    // 将当前状态发送给 activity
+    private void sendBroadToActivity() {
 
         Intent intent2 = new Intent("com.android.superplayer.Activity");
-        intent2.putExtra("state",state);
+        intent2.putExtra("state", state);
         sendBroadcast(intent2);
 
     }
 
 
-
     /*
      * 播放歌曲
      */
-    public void playMusic(MusicResult music){
-        if(player != null ){
+    public void playMusic(MusicResult music) {
+        if (player != null) {
             // 停止播放
             player.stop();
             player.reset();
@@ -113,6 +139,39 @@ public class MusicService extends Service {
                 player.prepare();
                 // 播放
                 player.start();
+
+                duration = player.getDuration();//获取当前歌曲时长
+
+
+                //将进度条 实时更新
+                //
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+
+                        while (curposition < duration) {
+
+                            try {
+                                // 每秒更新一下
+                                sleep(1000);
+
+                                curposition = player.getCurrentPosition();
+
+                                Intent intent = new Intent("com.android.superplayer.Activity");
+                                intent.putExtra("curposition", curposition);
+                                intent.putExtra("duration", duration);
+                                sendBroadcast(intent);
+                            } catch (Exception e) {
+
+
+                            }
+
+                        }
+
+
+                    }
+                }.start();
 
             } catch (Exception e) {
                 e.printStackTrace();
--- app/src/main/java/com/android/superplayer/ui/activity/my/MediaPlayerActivity.java
+++ app/src/main/java/com/android/superplayer/ui/activity/my/MediaPlayerActivity.java
@@ -121,6 +121,8 @@ public class MediaPlayerActivity extends BaseActivity {
         listView.setAdapter(musicAdapter);
         listView.setOnItemClickListener(oClickListener);
 
+        seekBarOnChange();
+
         MyBroadcastActivity receiver = new MyBroadcastActivity();
         IntentFilter intentFilter = new IntentFilter(flag_activity);
         registerReceiver(receiver, intentFilter);
@@ -132,7 +134,37 @@ public class MediaPlayerActivity extends BaseActivity {
 
     }
 
+    private void seekBarOnChange(){
 
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            // 当拖动条正在拖动 调用该方法
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            // 当开始 拖动 拖动条 后 调用该方法
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            // 当拖动条停止后 调用该方法
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Intent intent = new Intent("com.android.superplayer.Service");
+               // intent.putExtra("music", music);
+               // intent.putExtra("newmusic", 1);// 1 新音乐 ；
+                intent.putExtra("progress", seekBar.getProgress());//进度条位置 ；
+                sendBroadcast(intent);
+
+            }
+        });
+
+    }
+
+
+    // list 单项单击事件
     private AdapterView.OnItemClickListener oClickListener = new AdapterView.OnItemClickListener() {
 
         @Override
@@ -151,7 +183,7 @@ public class MediaPlayerActivity extends BaseActivity {
 
 
     /*
-     *
+     * 服务  发过来的广播
      */
     public class MyBroadcastActivity extends BroadcastReceiver {
         @Override
@@ -171,7 +203,42 @@ public class MediaPlayerActivity extends BaseActivity {
                     break;
             }
 
+            int curposition = intent.getIntExtra("curposition", -1);
+            int duration = intent.getIntExtra("duration", -1);
 
+            if(curposition != -1){
+               seekBar.setProgress( (int)(curposition*1.0 /duration * 100));
+
+
+               time.setText(inittime(curposition,duration));
+
+            }
+
+            boolean over = intent.getBooleanExtra("over", false);
+            if (over) {//
+                //播放模式  下一首
+                btnPlay.performClick() ;//触发事件
+
+            }
+        }
+
+    }
+    // 将毫秒 转为 分秒
+    private String inittime(int cur ,int dur){
+        int cur_fen = cur / 1000 / 60 ;// 分
+        int cur_miao = cur / 1000 % 60 ;// 秒
+
+        int dur_fen = cur / 1000 / 60 ;// 分
+        int dur_miao = cur / 1000 % 60 ;// 分
+
+        // 01：20  ，10：21
+        return  getT(cur_fen)+":" + getT(cur_miao)  + "/"  + getT(dur_fen)+":" + getT(dur_miao);
+    }
+    private String getT(int time){
+        if(time < 10){
+           return  "0" +time;
+        }else {
+            return  "" +time;
         }
 
     }
