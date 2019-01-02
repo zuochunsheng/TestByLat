8f6318c50bf825ddca41a55c2c66e0af0f265f3f
zuochunsheng <zuochunsheng@ledcas.com>
2019-01-02 21:01:04 +0800

    全局弹出框33

--- app/src/main/java/com/android/superplayer/ui/activity/test/AppFloatActionButtonActivity.java
+++ app/src/main/java/com/android/superplayer/ui/activity/test/AppFloatActionButtonActivity.java
@@ -1,29 +1,61 @@
 package com.android.superplayer.ui.activity.test;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.superplayer.R;
 import com.android.superplayer.base.BaseActivity;
 import com.android.superplayer.config.LogUtil;
 import com.android.superplayer.eventbus.EBBean;
 import com.android.superplayer.eventbus.EBConst;
+import com.android.superplayer.ui.activity.MainActivity;
 import com.android.superplayer.ui.widgit.FloatView2;
+import com.android.superplayer.ui.widgit.VerticalMarqueeView;
 import com.android.superplayer.util.ActivityUtil;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import app.demo.com.autotextlib.AutoVerticalScrollTextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class AppFloatActionButtonActivity extends BaseActivity {
+import static com.android.superplayer.util.Metrics.sp2px;
+
+public class AppFloatActionButtonActivity extends BaseActivity implements View.OnClickListener{
 
     @BindView(R.id.skip)
     TextView skip;
+    @BindView(R.id.vmView)
+    VerticalMarqueeView vmView;
+    @BindView(R.id.auto_text)
+    AutoVerticalScrollTextView auto_text;
+
+
     private FloatView2 floatView2;
+
+
+
+    int[] res = {R.id.imageView_a,R.id.imageView_b,R.id.imageView_c,R.id.imageView_d};
+    // a list of ImageViews
+    private List<ImageView> mImageViewList = new ArrayList<>();
+
+    // a flag to control opening action or closing action
+    private boolean opened = false;
+    // 轨迹半径
+    int r = 150;
+
+
+
 
     @Override
     protected int getLayoutId() {
@@ -46,7 +78,34 @@ public class AppFloatActionButtonActivity extends BaseActivity {
         EventBus.getDefault().register(this);
 
 
+        // 竖直方向 滚动textView
+        String[] datas = new String[]{
+                "南海又开始动荡了", "菲律宾到处都在肇事", "这次为了一张审判废纸，菲律宾投入了多少成本呢", "测试数据4", "测试数据5为了长度不一样", "就把这条当做测试数据吧"
+        };
+        vmView.color(getResources().getColor(android.R.color.black))
+                .textSize(sp2px(this, 15))
+                .datas(datas).commit();
+        vmView.startScroll();
+        vmView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AppFloatActionButtonActivity.this, "当前的索引为：" + vmView.getCurrentPosition(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        // 竖直方向 滚动textView  引用aar包
+        //auto_text = findViewById(R.id.auto_text);
+        auto_text.setTextSources("一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十");
+//        auto_text.setTextSources("1234567890123456789012345678901234567890");
+        auto_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //扇形初始化
+        initView();
     }
 
     //eventbug接受消息 根据类别区分处理 threadMode = ThreadMode.POSTING
@@ -56,7 +115,8 @@ public class AppFloatActionButtonActivity extends BaseActivity {
             LogUtil.e("AppFloatActionButtonActivity === showFloatView");
             floatView2.showFloatView();
 
-        }if (ebBean.getValue() == EBConst.app_stop) {
+        }
+        if (ebBean.getValue() == EBConst.app_stop) {
             LogUtil.e("AppFloatActionButtonActivity === hideFloatView");
             floatView2.hideFloatView();
 
@@ -69,11 +129,99 @@ public class AppFloatActionButtonActivity extends BaseActivity {
     }
 
 
-
-
     @OnClick(R.id.skip)
     public void onViewClicked() {
-        ActivityUtil.getInstance().onNext(this,ViewPageIndicatorActivity.class);
+        ActivityUtil.getInstance().onNext(this, ViewPageIndicatorActivity.class);
 
     }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //必须要调用，否则内存中会一直无限循环
+        vmView.stopScroll();
+
+        auto_text.destroyView();
+
+    }
+
+
+
+
+    //控件初始化
+    private void initView() {
+        for(int i=0;i<res.length;i++){
+            ImageView imageView = (ImageView) findViewById(res[i]);
+            mImageViewList.add(imageView);
+            imageView.setOnClickListener(this);
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imageView_a:
+                if(!opened) {
+                    openAnimator();
+                } else {
+                    closeAnimator();
+                }
+                break;
+            default:
+                Toast.makeText(v.getContext(),"Id: "+v.getId(),Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+
+    //控件关闭
+    private void closeAnimator() {
+        for(int i=0;i<res.length-1;i++){
+            //计算每个控件展开的弧度
+            moveBack(mImageViewList.get(i+1),(float) (Math.PI/2/(res.length-2))*(i+1));
+        }
+        opened = false;
+    }
+
+    // 控件打开
+    private void openAnimator() {
+        for(int i=0;i<res.length-1;i++){
+            //计算每个控件展开的弧度
+            moveTo(mImageViewList.get(i+1),(float) (Math.PI/2/(res.length-2))*(i+1));
+        }
+        opened = true;
+    }
+
+    /**
+     * 扇形展开
+     * @param objView 目标控件
+     * @param angle 展开的角度
+     */
+    void moveTo(View objView, float angle){
+//        ObjectAnimator translationX = ObjectAnimator.ofFloat(objView, "translationX", 0f, (float) Math.cos(angle) * r);
+//        ObjectAnimator translationY = ObjectAnimator.ofFloat(objView, "translationY", 0f, (float) Math.sin(angle) * r);
+
+        ObjectAnimator translationX = ObjectAnimator.ofFloat(objView, "translationX", 0f, -(float) Math.cos(angle) * r);
+        ObjectAnimator translationY = ObjectAnimator.ofFloat(objView, "translationY", 0f, -(float) Math.sin(angle) * r);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(translationX,translationY);
+        animatorSet.setDuration(500).start();
+    }
+
+    /**
+     * 扇形关闭
+     * @param objView 目标控件
+     * @param angle 关闭的角度
+     */
+    void moveBack(View objView, float angle){
+        ObjectAnimator translationX = ObjectAnimator.ofFloat(objView, "translationX", -(float) Math.cos(angle) * r,0f);
+        ObjectAnimator translationY = ObjectAnimator.ofFloat(objView, "translationY", -(float) Math.sin(angle) * r,0f);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(translationX,translationY);
+        animatorSet.setDuration(500).start();
+    }
+
+
 }
--- app/src/test/java/com/android/superplayer/ExampleUnitTest.java
+++ app/src/test/java/com/android/superplayer/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.android.superplayer;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
@@ -14,6 +16,146 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+
+    @Test
+    public void checkTest() {
+        int[] strs = {1,4,3,6};
+
+        int [][] array= {{1, 2, 8, 9},
+                         {2, 4, 9, 12},
+                         {4, 7, 10, 13},
+                         {6, 8, 11, 15}};
+
+        boolean b = find(array, 8);
+        System.out.println(b);
+        assertEquals(4, 2 + 2);
+
+    }
+
+    @Test
+    public void checkSortTest(){
+        int[] data = {8,6,1,4,7,2,9,6,4} ;
+
+        //sort(data);
+        partitionArray(data);
+        System.out.println(data);
+        System.out.println(Arrays.toString(data));
+
+
+        assertEquals(4, 2 + 2);
+    }
+
+
+    // 替换空格为指定字符
+    private String replaceBlankSpace(String strings){
+
+        if(strings!=null && strings.trim()!="") {
+            //方法一
+            //return strings.replaceAll("\\s", "%20");// " "
+
+            //方法二
+            StringBuffer stringBuffer = new StringBuffer(strings);
+            for (int i = stringBuffer.length() -1; i >=0 ; i--) {// 从后往前
+                char c = stringBuffer.charAt(i);
+                String str = String.valueOf(c);
+
+                if(str.equals(" ")){
+                    stringBuffer.replace(i ,i+1,"%20");
+
+                }
+
+
+            }
+            return stringBuffer.toString();
+           
+
+        }
+        return "";
+    }
+
+
+    //java实现在二维数组中查找一个数
+    public boolean find(int[][] array,int target ) {
+        int rows = array.length;
+        int columns = array[0].length;
+
+        boolean found = false ;
+
+        if(array!=null && rows >0 && columns>0){
+            int row = 0 ;
+            int column = columns-1;
+            while (row <rows && column >= 0){
+
+                if (array[row][column] == target){
+                    found = true;
+                    break;
+
+                }
+                else if (array[row][column] > target){
+                    --column ;
+                }else {
+                        ++row ;
+                }
+            }
+
+
+        }
+        return  found;
+
+    }
+
+
+
+
+    //偶数顺序改变
+    private void sort(int[]  data){
+        for (int i = 0; i < data.length-1; i++) {
+            if( data[i] %2 == 0){ //能被2整除的数都是偶数，反之为奇数
+
+                int j = i + 1;
+                while (j < data.length) {
+                    if (data[j] % 2 != 0) {
+                        int temp = data[i];
+                        data[i] = data[j];
+                        data[j] = temp;
+                        break;
+                    }
+                    j++;
+                }
+                //说明后面的全部均为偶数，没必须要往下循环。
+                if (j == data.length) {
+                    break;
+                }
+
+
+            }
+
+        }
+
+
+    }
+
+    public void partitionArray(int[] nums) {
+        // write your code here;
+        int i=0;
+        int j=nums.length - 1;
+        while(i<j){
+            while(nums[i]%2==1){
+                i++;
+            }
+            while(nums[j]%2==0){
+                j--;
+            }
+            if(i<j){
+                nums[i]=nums[i]+nums[j];
+                nums[j]=nums[i]-nums[j];
+                nums[i]=nums[i]-nums[j];
+            }
+
+        }
+    }
+
 
 
 
